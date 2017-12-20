0ee92a1b28d9e034f6f0c1e78880341cb8542bd9
Jong-Hyuk Ellis Cho <ellisjoe611@gmail.com>
2017-12-20 20:56:35 +0900

    Edited some Admin files...

--- administrator/admin_product_action.php
+++ administrator/admin_product_action.php
@@ -14,23 +14,22 @@ echo $object -> get_data_in_table("SELECT * FROM product ORDER BY p_id ASC");
 //값들을 삽입한다
 if($_POST["action"] == "Insert")
 {
-	$p_id = mysqli_real_escape_string($object ->connect, $_POST["p_id"]);
-	$p_num = mysqli_real_escape_string($object ->connect, $_POST["p_num"]);
-	$cate1 = mysqli_real_escape_string($object ->connect, $_POST["cate1"]);
-	$cate2 = mysqli_real_escape_string($object ->connect, $_POST["cate2"]);
-	$name = mysqli_real_escape_string($object ->connect, $_POST["name"]);
-	$expiry_day = mysqli_real_escape_string($object ->connect, $_POST["expiry_day"]);
-	$price = mysqli_real_escape_string($object ->connect, $_POST["price"]);
-	$like_hate = mysqli_real_escape_string($object ->connect, $_POST["like_hate"]);
-	$productPhoto = $object -> upload_file($_FILES["productPhoto"]);
+   $p_id = mysqli_real_escape_string($object ->connect, $_POST["p_id"]);
+   $p_num = mysqli_real_escape_string($object ->connect, $_POST["p_num"]);
+   $cate1 = mysqli_real_escape_string($object ->connect, $_POST["cate1"]);
+   $cate2 = mysqli_real_escape_string($object ->connect, $_POST["cate2"]);
+   $name = mysqli_real_escape_string($object ->connect, $_POST["name"]);
+   $expiry_day = mysqli_real_escape_string($object ->connect, $_POST["expiry_day"]);
+   $price = mysqli_real_escape_string($object ->connect, $_POST["price"]);
+   $productPhoto = $object -> upload_file($_FILES["productPhoto"]);
 
-	//activate query(모은 정ㅂ들에 대해서 쿼리를 작동한다)
-	$query = "INSERT INTO product (p_id, p_num, cate1, cate2, name, expiry_day, price, like_hate, productPhoto) 
-	VALUES ('".$p_id."','".$p_num."', '".$cate1."', '".$cate2."', '".$name."', '".$expiry_day."', '".$price."', '".$like_hate."', '".$productPhoto."')";
+   //activate query(모은 정ㅂ들에 대해서 쿼리를 작동한다)
+   $query = "INSERT INTO product (p_id, p_num, cate1, cate2, name, expiry_day, price, productPhoto)
+   VALUES ('".$p_id."','".$p_num."', '".$cate1."', '".$cate2."', '".$name."', '".$expiry_day."', '".$price."', '".$productPhoto."')";
 
-	$object -> execute_query($query);
+   $object -> execute_query($query);
 
-	echo 'Add complete';
+   echo 'Add complete';
 }
 
 //수정
@@ -42,16 +41,15 @@ $result = $object -> execute_query($query);
 
 //값들을 display한다
 while($row = mysqli_fetch_array($result))
-{ 
-output["p_id"] = $row["p_id"];
+{
+$output["p_id"] = $row["p_id"];
 $output["p_num"] = $row["p_num"];
 $output["cate1"] = $row["cate1"];
 $output["cate2"] = $row["cate2"];
 $output["name"] = $row["name"];
 $output["expiry_day"] = $row["expiry_day"];
 $output["price"] = $row["price"];
-$output["like_hate"] = $row["like_hate"];
-$output["productPhoto"] = '<img src="upload/'.$row["productPhoto"].'" class="img-thumbnail" width="50" height="35" />';
+$output["productPhoto"] = '<img src="/myservice/images/product/'.$row["productPhoto"].'" class="img-thumbnail" width="50" height="35" />';
 }
 
 echo json_encode($output);
@@ -81,10 +79,9 @@ $cate2 = mysqli_real_escape_string($object ->connect, $_POST["cate2"]);
 $name = mysqli_real_escape_string($object ->connect, $_POST["name"]);
 $expiry_day = mysqli_real_escape_string($object ->connect, $_POST["expiry_day"]);
 $price = mysqli_real_escape_string($object ->connect, $_POST["price"]);
-$like_hate = mysqli_real_escape_string($object ->connect, $_POST["like_hate"]);
 
 $query = "UPDATE product SET p_id ='".$p_id."' , p_num = '".$p_num."', cate1 = '".$cate1."' , cate2 = '".$cate2."'
-, name = '".$name."' , expiry_day = '".$expiry_day."' , price = '".$price."' , like_hate = '".$like_hate."' , productPhoto = '".$productPhoto."' 
+, name = '".$name."' , expiry_day = '".$expiry_day."' , price = '".$price."' , productPhoto = '".$productPhoto."'
 WHERE p_id = '".$_POST["product_id"]."' ";
 $object -> execute_query($query);
 echo "수정 성공";
